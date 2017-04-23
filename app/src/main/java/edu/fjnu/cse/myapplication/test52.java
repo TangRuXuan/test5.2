3f128d8a614fa907564a3c375aeb7cc6ee8f2015
TangRuxuan <435750608@qq.com>
2017-04-23 17:35:45 +0800

    Initial commit

---
+++ app/src/main/java/edu/fjnu/cse/myapplication/test52.java
@@ -0,0 +1,30 @@
+package edu.fjnu.cse.myapplication;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class test52 extends AppCompatActivity {
+
+    private WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test52);
+
+        web= (WebView) findViewById(R.id.web);
+
+        web.loadUrl("http://www.baidu.com");
+    }
+}
+
+
+
+
